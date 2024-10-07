a6508c02edfc2cdbb5b4c480755a77c6de1c3358
Alejandro Viño Loureiro <alejandrovl@ext.inditex.com>
2024-10-07 09:24:10 +0200

    feat: adapt spacings to mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import Header from '@/components/Header/Header';
 import { Montserrat } from 'next/font/google';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 const montserrat = Montserrat({
   subsets: ['latin'],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,33 +9,33 @@ export default function Home() {
   return (
     <div className="h-[calc(100dvh_-_80px)] w-full flex flex-col justify-between">
       <Image
-        className="absolute top-0 mx-auto max-w-[100dvw] max-h-[100dvh] h-auto w-auto"
+        className="absolute top-16 sm:top-0 mx-auto max-w-[100dvw] max-h-[100dvh] h-auto w-auto"
         src={blurry}
         alt="blurry background"
       />
-      <div className="flex flex-col mt-20 mx-auto md:mx-0 md:ml-10 xl:ml-20 2xl:ml-28 z-[1]">
+      <div className="flex flex-col mt-14 sm:mt-20 mx-auto md:mx-0 md:ml-10 xl:ml-20 2xl:ml-28 z-[1]">
         <h1
-          className={`text-7xl lg:text-8xl xl:text-9xl ml-16 font-[500] ${styles.nohemiRegular}`}>
+          className={`text-7xl lg:text-8xl xl:text-9xl ml-12 sm:ml-16 font-[500] ${styles.nohemiRegular}`}>
           Ciencia
         </h1>
         <p
           className={`text-6xl lg:text-7xl xl:text-8xl ${styles.fanwoodItalic}`}>
-          hecha <span className="underline decoration-[#FFED9D]">fácil!</span>
+          hecha <span className="underline decoration-[#FCD655]">fácil!</span>
         </p>
       </div>
       <div className=" w-full self-center flex flex-wrap sm:flex-nowrap sm:flex-row items-start sm:items-end gap-2 md:gap-5 justify-center h-2/5 sm:h-1/2 pb-5">
         <Card
-          className="w-full sm:w-1/3 h-[40%] sm:h-4/5 bg-[#DAFFB9]"
+          className="w-full sm:w-1/3 h-[40%] sm:h-4/5 bg-[#BDFB52]"
           title="Primaria"
           description="hey"
         />
         <Card
-          className="w-[48%] sm:w-1/3 h-[60%] sm:h-full self-stretch bg-[#FFED9D]"
+          className="w-[48%] sm:w-1/3 h-[60%] sm:h-full self-stretch bg-[#FCD655]"
           title="Secundaria"
           description="hey"
         />
         <Card
-          className="w-[48%] sm:w-1/3 h-[60%] sm:h-2/3 self-stretch sm:self-auto bg-[#9DE0FF]"
+          className="w-[48%] sm:w-1/3 h-[60%] sm:h-2/3 self-stretch sm:self-auto bg-[#1A63F7]"
           title="Bachillerato"
           description="hey"
         />
